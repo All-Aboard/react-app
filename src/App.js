91d76f267cc00166a323ce1f9dd49ec00c0f9b39
Kyle <the.big.kb123@gmail.com>
2018-09-08 14:43:28 +0200

    wallet in localstorage, login working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Btn from './components/Button';
 import TextField from '@material-ui/core/TextField';
+import { LOADING } from './StyleGuide';
 import './App.css';
 
 const ethers = require('ethers');
@@ -13,8 +14,17 @@ class App extends Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      wallet: '',
+      isCreatingWallet: false
     };
+  }
+
+  componentDidMount(){
+    var storedWallet = localStorage.getItem('wallet');
+    if (storedWallet) {
+      this.setState({wallet: storedWallet, username: localStorage.getItem('username')});
+    }
   }
 
   handleUsernameChange(e) {
@@ -25,19 +35,23 @@ class App extends Component {
     this.setState({password: e.target.value});
   }
 
-  createEthAddress(e) {
-    // TODO: Brain wallet isn't working?
-    // ethers.Wallet.fromBrainWallet(this.state.username, this.state.password).then(function(wallet) {
-    //   console.log("Address: " + wallet.address);
-    // });
-    var account = ethers.Wallet.createRandom();
-    console.log("Created ETH Address: " + JSON.stringify(account));
+  async createEthAddress(e) {
+    this.setState({isCreatingWallet: true});
+    var newWallet;
+    await ethers.Wallet.fromBrainWallet(this.state.username, this.state.password).then(function(wallet){
+      console.log(wallet)
+      newWallet = wallet;
+    })
+    this.setState({wallet: newWallet, isCreatingWallet: false});
+    localStorage.setItem('username', this.state.username);
+    localStorage.setItem('wallet', this.state.wallet);
   }
 
 
   render() {
-    return (
-      <div className="App">
+
+    var login =
+    <div> 
         <div>
           <TextField onChange={this.handleUsernameChange.bind(this)} placeholder="Username"/>
         </div> 
@@ -45,8 +59,24 @@ class App extends Component {
           <TextField type='Password' onChange={this.handlePasswordChange.bind(this)} placeholder="Password"/>
         </div>
         <div>
-          <Btn primary onClick={this.createEthAddress.bind(this)} type="submit">Create Account </Btn>
+          <Btn primary onClick={this.createEthAddress.bind(this)} type="submit"> Create Account </Btn>
         </div>
+    </div>  
+
+    if (this.state.wallet === '' && this.state.isCreatingWallet === true) {
+      login = <LOADING> <i className="fa fa-circle-o-notch fa-spin"></i> Creating Account... </LOADING>
+    }
+    if (this.state.wallet !== '') {
+      login = <div>Welcome, {this.state.username}</div>
+    }
+
+
+
+
+
+    return (
+      <div className="App">
+        {login}
       </div>
     );
   }
